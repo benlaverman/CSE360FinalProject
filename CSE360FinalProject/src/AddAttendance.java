6bef5213aea0986f85150da398b3424946953f27
dmcgreg <mcgregor.damian@gmail.com>
2020-11-25 02:01:28 -0700

    Changed AddAttendance, added LoadRoster, Student, and Main for testing.

--- CSE360FinalProject/src/AddAttendance.java
+++ CSE360FinalProject/src/AddAttendance.java
@@ -1,28 +1,45 @@
+
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class AddAttendance {
+public class AddAttendance extends Observable {
+	protected ArrayList<Student> Roster;
 	
-	protected ArrayList AddAttendance(ArrayList<String[]> list, File file) throws IOException {
-		ArrayList<String[]> newList = new ArrayList<String[]>();		// create ArrayList of String array
+	public AddAttendance()  {
+		Roster = new ArrayList<Student>();
+	}
+	
+	public void addAttendance(File file, ArrayList<Student> newRoster) throws IOException {
+		/* Get date from user*/
+		Roster = newRoster;
+		
 		String row;
 		
 		BufferedReader csvReader = new BufferedReader(new FileReader(file));		// buffer read the file
 		while ((row = csvReader.readLine()) != null) {		// read each line in the file
-		    String[] data = row.split(",");		// separate each line by commas 
-		    newList.add(data);		// add separated data to LinedList
+		    String[] data = row.split(",");		// separate each line by commas
+		    
+		    int count = 0;
+		    while(count < Roster.size() - 1 && !data[0].equals(Roster.get(count).getAsurite())) {		// search for matching asurite
+		    	//System.out.println(data[0] + "\t\t" + Roster.get(count).getAsurite());
+		    	count++;
+		    }
+
+		    // if asurite matches, set new time
+	    	if (data[0].equals(Roster.get(count).getAsurite())) {
+	    		//System.out.print("TEST");
+	    		int newTime = Roster.get(count).getTime() + Integer.parseInt(data[1]);
+	    		Roster.get(count).setTime(newTime);
+	    	}
 		}
-		csvReader.close();		// close file reader
 		
-		/*
-		 * Compare for matching data in file!
-		 */
-		
-		// add all of the new data to the list
-		for (int i = 0; i < newList.size() - 1; i++) {
-			list.add(newList.get(i));
-		}
-		
-		return list;
+		setChanged(); 
+        notifyObservers();
+	}
+	
+	public ArrayList<Student> getRoster() {
+		return Roster;
 	}
 }
